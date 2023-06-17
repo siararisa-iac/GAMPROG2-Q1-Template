efca7267e7dff4b7e25d4ef96986258704679b2f
siararisa-iac <126797704+siararisa-iac@users.noreply.github.com>
2023-06-17 08:08:11 +0800

    Added jump to player character controller

--- Assets/Scripts/Movement/CharacterControllerMovement.cs
+++ Assets/Scripts/Movement/CharacterControllerMovement.cs
@@ -8,8 +8,12 @@ public class CharacterControllerMovement : MonoBehaviour
     private float moveSpeed = 2.0f;
     [SerializeField]
     private float gravityScale = 1.0f;
+    [SerializeField]
+    private float jumpHeight = 3.0f;
 
     private float gravity = -9.8f;
+    private float groundedVelocity = -0.5f;
+    private float yVelocity;
 
     private CharacterController characterController;
 
@@ -25,14 +29,25 @@ public class CharacterControllerMovement : MonoBehaviour
 
     private void Move()
     {
-       float xMove = Input.GetAxis("Horizontal");
-       float zMove = Input.GetAxis("Vertical");
-
-        Vector3 moveDirection = (transform.right * xMove) + (transform.forward * zMove);
-        moveDirection.y += gravity * Time.deltaTime * gravityScale;
-        moveDirection *= moveSpeed * Time.deltaTime;
-       
-        //Debug.Log(moveDirection);
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime;
+        moveDirection += GetGravityAndJump();
         characterController.Move(moveDirection);
+    }
+
+    private Vector3 GetGravityAndJump()
+    {
+        //Ensure that the character is kept grounded
+        if (characterController.isGrounded && yVelocity < 0f)
+        {
+            yVelocity = groundedVelocity;
+        }
+        if(characterController.isGrounded && Input.GetKeyDown(KeyCode.Space)) 
+        {
+            // Calculate the velocity given the height
+            yVelocity = Mathf.Sqrt(jumpHeight * 2.0f * Mathf.Abs(gravity));
+        }
+        // Keep applying gravitational force
+        yVelocity += gravity * gravityScale * Time.deltaTime;
+        return Vector3.up * yVelocity * Time.deltaTime;
     }
 }
